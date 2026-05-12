6d6c76cfefbb55caa30384c5a6517a104d8aee10
Josu Goitia <j.goitia@digipen.edu>
2026-05-12 12:55:39 +0200

    MongoDB Atlas conection

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Ruta de prueba
 app.get('/', (req, res) => {
   res.json({ message: '🚀 API funcionando correctamente' });
 });
